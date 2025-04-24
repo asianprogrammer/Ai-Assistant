999199a8a96bdb08d5b3d53815d71486e5376e7b
Asian Programmer <parveznew2022@gmail.com>
2025-04-24 09:47:39 +0600

    Debugg Noejs App

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hello, World")
+let input = document.getElementById('input')
+let submit = document.getElementById('submit')
+var data = process.env.API_KEY;
+
+submit.addEventListener('click', function() {
+    console.log(input.value)
+    console.log(data)
+})
